a276f8ba4158c612e626c41792639f0cd9df0ff5
IkerGalardi <contacto.ikergalardi@gmail.com>
2021-02-19 11:16:39 +0100

    C++ global constructors called

--- lib/start.cc
+++ lib/start.cc
@@ -2,11 +2,20 @@
 
 extern void main(int argc, char** argv);
 
+typedef void (*ctor)();
+extern "C" ctor start_ctors;
+extern "C" ctor end_ctors;
+
 extern "C" 
 void gimme_all_your_lovin(void)
 {
 	steel::uart_init();
 	steel::uart_send_string("Platform -> Raspberry PI 3\r\n");
+
+	// Call all global constructors
+	for(ctor* constructor = &start_ctors; constructor != &end_ctors; constructor++) {
+        (*constructor)();
+    }
 
 	main(0, nullptr);
 }
