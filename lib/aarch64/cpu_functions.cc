d2a87b530c448e6693d251244f66c41dd8726d00
IkerGalardi <contacto.ikergalardi@gmail.com>
2021-03-09 16:43:26 +0100

    stack pointer shared with EL0

---
+++ lib/aarch64/cpu_functions.cc
@@ -0,0 +1,5 @@
+#include "cpu_functions.hh"
+
+namespace steel {
+
+}
---
+++ lib/aarch64/cpu_functions.hh
@@ -0,0 +1,9 @@
+#pragma once
+
+namespace steel {
+    void instruction_sync_barrier();
+
+    void data_memory_barrier();
+
+    void data_sync_barrier();
+}
